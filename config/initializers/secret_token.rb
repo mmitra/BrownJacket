8fb99425093e2b7742b4d95250e726792b90b514
Monty Mitra <mmitra@engineyard.com>
2011-09-19 14:46:30 -0700

    first comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrownJacket::Application.config.secret_token = 'd471a5ce499c75b14563579188a2151fdec861104e1d2678fa1e4a1c961607bafb9dd02e11d8a5040df0caec57840d27850187b6e2382984f0e7f3906ae73983'
